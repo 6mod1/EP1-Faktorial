9139106a5ea032738e51ac700bfd778755f872af
David Hráček <158260270+6mod1@users.noreply.github.com>
2024-06-11 08:49:29 +0200

    Update Program.vb

--- EP1-Faktorial/Program.vb
+++ EP1-Faktorial/Program.vb
@@ -1,7 +1,36 @@
 Imports System
 
 Module Program
-  Sub Main(args As String())
-    Console.WriteLine("Hello World!")
-  End Sub
+    Sub Main(args As String())
+        Dim vstup As String
+        Dim cislo As Integer
+        Dim soucet As Integer = 0
+        Do
+            Console.Write("Zadej èíslo: ")
+            vstup = Console.ReadLine
+
+            If Integer.TryParse(vstup, cislo) Then
+                If cislo < 10 Then
+                    If cislo > 0 Then
+                        For i = 1 To cislo
+                            soucet = soucet + i
+                        Next
+                        Console.WriteLine($"Souèet od 1 do {cislo} je {soucet}")
+                    Else
+                        Console.WriteLine("Èislo nesmí být záporné!")
+                    End If
+                Else
+                        Console.WriteLine("Zadej èíslo menší než 10!")
+                End If
+
+            Else
+                Console.WriteLine("Napiš jen celé èíslo!")
+                End If
+
+
+        Loop
+
+
+
+    End Sub
 End Module
